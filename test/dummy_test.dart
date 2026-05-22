118d98e3fc60eed20767159e5760a1559df081f5
Lars Patzenbein <203286525+Exoshiva@users.noreply.github.com>
2026-05-22 13:17:03 +0200

    test: add dummy test file to initialize test directory for CI pipeline

---
+++ test/dummy_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Pipeline Dummy Test', () {
+    // Dieser Test ist nur da, damit die CI-Pipeline nicht abbricht, 
+    // bis die echten JSON- und Logik-Tests geschrieben werden.
+    expect(true, true);
+  });
+}
